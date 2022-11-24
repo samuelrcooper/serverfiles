0aef7fc4141d0888205e3cab284ccff2b20f07c4
Fabricio <contact@fabricioguardia.com>
2022-11-23 19:45:28 -0500

    create playlist and add items controller and routes

--- controllers/generator.js
+++ controllers/generator.js
@@ -103,3 +103,66 @@ exports.playlistTracks = async (req, res) => {
   })
   
 }
+
+exports.createPlaylist = (req, res) => {
+
+  const form = formidable({ multiples: true })
+
+  form.parse(req, async (err, fields, files) => {
+    
+    parseOnly(fields)
+
+    const headerOptions = {
+      Accept: 'application/json',
+      ContentType: 'application/json',
+      Authorization: `Bearer ${fields.token}`,
+    }
+
+    let uris = []
+
+    fields.tracks.forEach((item, idx) => {
+      if(item.track.uri) uris.push(item.track.uri)
+    })
+
+    // console.log(uris.length)
+
+    try {
+        
+      const response = await axios.post(`https://api.spotify.com/v1/users/${fields.userID}/playlists`, 
+        {
+          name: fields.playlistName,
+          description: ''
+        },
+        { headers: headerOptions }
+      )
+
+      if(response.data.id){
+        
+        const responseAddItems = await axios.post(`https://api.spotify.com/v1/playlists/${response.data.id}/tracks`, 
+          { "uris": uris }, 
+          { headers: headerOptions }
+        )
+        
+      }
+
+      return res.json('Playlist was generated')
+
+      
+    } catch (error) {
+      console.log(error.response)
+
+      if(error.response.data){
+        if(error.response.data.error){
+          if(error.response.data.error.status === 401){
+            return res.status(401).json(error.response.data.error.message)
+          }
+        }
+      }
+
+      return res.status(400).json(error.response.data ? error.response.data : 'Error with spotify service')
+      
+    }
+
+  })
+  
+}
--- routes/generator.js
+++ routes/generator.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { login, callback, playlistTracks } = require('../controllers/generator')
+const { login, callback, playlistTracks, createPlaylist } = require('../controllers/generator')
 
 router.get('/login', login)
 router.get('/callback', callback)
 router.post('/playlist-tracks', playlistTracks)
+router.post('/create-playlist', createPlaylist)
 
 module.exports  = router
